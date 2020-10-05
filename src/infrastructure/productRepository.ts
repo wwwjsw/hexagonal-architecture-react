fbe55ca37b365beb78c127887d5b33018c11a390
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-10-05 11:06:43 -0300

    change product repo

--- src/infrastructure/productRepository.ts
+++ src/infrastructure/productRepository.ts
@@ -3,7 +3,7 @@ import {Product} from "../domain/entities"
 
 export class ProductRepository implements IProductRepository {
     async getProducts(): Promise<Array<Product>> {
-        const res = await fetch("http://url")
+        const res = await fetch("https://5f7a81224ebc4100161cada8.mockapi.io/api/v1/products")
         return res.json()
     }
 }
